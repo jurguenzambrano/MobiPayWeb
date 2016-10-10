56a411bcedce4be02c7053e984cca81981365199
Juan Pedro Barriga Rivera <juanp1812@hotmail.com>
2016-10-10 01:54:26 -0500

    RecargaWeb
    Controller, Recarga

--- DSD_Mobipay/Controllers/BankController.cs
+++ DSD_Mobipay/Controllers/BankController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DSD_Mobipay.wsBankOperation;
+using System.Web.Script.Serialization;
+using System.Net;
+using System.IO;
 
 namespace DSD_Mobipay.Controllers
 {
@@ -13,45 +16,63 @@ namespace DSD_Mobipay.Controllers
         public string RealizarRecargaMonedero(string xCodigoUsuario, decimal mMonto)
         {
 
-            var wsMobipay = new wsBankOperation.BankOperationClient();
+            //var wsMobipay = new wsBankOperation.BankOperationClient();
 
-            Guid gBanco = Guid.Empty;
-            try
-            {
-                string ownerId = "89458299-D68E-435E-8369-5BC9D9C85DCD";
-                gBanco = new Guid(ownerId);
-            }
-            catch
-            {
-                // implement catch 
-            }
+            //Guid gBanco = Guid.Empty;
+            //try
+            //{
+            //    string ownerId = "89458299-D68E-435E-8369-5BC9D9C85DCD";
+            //    gBanco = new Guid(ownerId);
+            //}
+            //catch
+            //{
+            //    // implement catch 
+            //}
 
-            Random rnd = new Random();
-            long iOperacionBanco = rnd.Next(10000, 99999);
+            //Random rnd = new Random();
+            //long iOperacionBanco = rnd.Next(10000, 99999);
 
-            var movimientoBE = wsMobipay.refillwallet(gBanco, xCodigoUsuario, "00000" + iOperacionBanco.ToString(), mMonto);
+            //var movimientoBE = wsMobipay.refillwallet(gBanco, xCodigoUsuario, "00000" + iOperacionBanco.ToString(), mMonto);
 
-            return movimientoBE.lError + "|" + movimientoBE.xRespuesta;
+            //return movimientoBE.lError + "|" + movimientoBE.xRespuesta;
+            return "";
         }
 
         [ValidateAntiForgeryToken]
         public string BuscarUsuario(string xCodigoUsuario)
         {
+            HttpWebRequest req;
+            StreamReader reader;
+            string usuarioJson;
+            HttpWebResponse res;
+            JavaScriptSerializer js;
 
-            var wsMobipay = new wsBankOperation.BankOperationClient();
-            Guid gBanco = Guid.Empty;
+            // Obtener Usuario
+            var xURLService = "http://mobipayservice.apphb.com/UsuariosService.svc/usuarios/" + xCodigoUsuario;
+            req = (HttpWebRequest)WebRequest.Create(xURLService);
+            req.Method = "GET";
+            UsuarioBE UsuarioResult = new UsuarioBE();
+            string mensajeResult = "";
             try
             {
-                string ownerId = "89458299-D68E-435E-8369-5BC9D9C85DCD";
-                gBanco = new Guid(ownerId);
+                res = (HttpWebResponse)req.GetResponse();
+                reader = new StreamReader(res.GetResponseStream());
+                usuarioJson = reader.ReadToEnd();
+                js = new JavaScriptSerializer();
+                UsuarioResult = js.Deserialize<UsuarioBE>(usuarioJson);
+                mensajeResult = "True";
             }
-            catch
+            catch (WebException e)
             {
+                HttpStatusCode code = ((HttpWebResponse)e.Response).StatusCode;
+                string message = ((HttpWebResponse)e.Response).StatusDescription;
+                reader = new StreamReader(e.Response.GetResponseStream());
+                string error = reader.ReadToEnd();
+                js = new JavaScriptSerializer();
+                mensajeResult = js.Deserialize<string>(error);           
             }
 
-            var UsuarioBE = wsMobipay.getUser(gBanco, xCodigoUsuario);
-
-            return UsuarioBE.lError + "|" + UsuarioBE.xNombres + ' ' + UsuarioBE.xApellidos + '|' + UsuarioBE.xDocumento;
+            return mensajeResult + "|" + UsuarioResult.Nombres + ' ' + UsuarioResult.Apellidos + '|' + UsuarioResult.Dni;
         }        
         
         public ActionResult BankOperations()
@@ -70,4 +91,40 @@ namespace DSD_Mobipay.Controllers
         }
 
     }
+
+    public class UsuarioBE
+    {   
+        public string Dni { get; set; }
+
+        
+        public string Apellidos { get; set; }
+
+        
+        public string Nombres { get; set; }
+
+        
+        public string Direccion { get; set; }
+
+        
+        public string Celular { get; set; }
+
+        
+        public string Mail { get; set; }
+
+        
+        public string Estado { get; set; }
+
+        
+        public string Clave { get; set; }
+
+        
+        public string Codigocliente { get; set; }
+
+        
+        public decimal MontoMaximo { get; set; }
+
+        
+        public decimal Saldo { get; set; }
+    }
+
 }
